917897f70667f18baa0d691dc491ea0d92374a6d
Metin Şaylan <metinsaylan@gmail.com>
2021-02-06 22:06:16 +0300

    listen localhost only

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = require( './config/express' );
 require( './config/mongoose' );
 
 if ( !module.parent ) {
-  app.listen( config.port || 5600, () => {
+  app.listen( config.port || 5600, 'localhost', () => {
     console.info( `Server started on port ${config.port} (${config.env})` );
   } );
 }
